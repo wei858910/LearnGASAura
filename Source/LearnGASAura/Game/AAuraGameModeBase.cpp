96fbb5445c5cb6adba5ef9957d9bcc0267019fa8
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-12 19:48:18 +0800

    Aura Game Mode

---
+++ Source/LearnGASAura/Game/AAuraGameModeBase.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AAuraGameModeBase.h"
+
+#include "LearnGASAura/Player/AuraPlayerController.h"
+
+AAAuraGameModeBase::AAAuraGameModeBase()
+{
+    if (const ConstructorHelpers::FClassFinder<AAuraPlayerController> AuraPlayerControllerClass(TEXT("/Game/Player/BP_AuraPlayerController.BP_AuraPlayerController_C")); IsValid(AuraPlayerControllerClass.Class))
+    {
+        PlayerControllerClass = AuraPlayerControllerClass.Class;
+    }
+}
---
+++ Source/LearnGASAura/Game/AAuraGameModeBase.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AAuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNGASAURA_API AAAuraGameModeBase : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    AAAuraGameModeBase();
+};
