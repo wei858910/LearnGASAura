84c260bc93514e71ece53c020647c637c2b229ef
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-13 19:51:02 +0800

    Replication Mode 复制模式

--- Source/LearnGASAura/Character/Enemy/AuraEnemy.cpp
+++ Source/LearnGASAura/Character/Enemy/AuraEnemy.cpp
@@ -13,6 +13,11 @@ AAuraEnemy::AAuraEnemy()
 
     AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
     AbilitySystemComponent->SetIsReplicated(true);
+    // 设置能力系统组件的游戏玩法效果复制模式为最小化模式（Minimal）。
+    // 在最小化模式下，服务器仅会复制必要的游戏玩法效果信息到客户端，
+    // 主要复制游戏玩法效果的持续时间、堆栈数量等基本信息，而不会复制详细的预测信息，
+    // 这样能显著减少网络带宽的使用，但可能会使客户端的游戏玩法效果表现与服务器略有延迟或差异。
+    AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 
     AttributeSet = CreateDefaultSubobject<UAttributeSet>(TEXT("AttributeSet"));
 }
--- Source/LearnGASAura/Player/AuraPlayerState.cpp
+++ Source/LearnGASAura/Player/AuraPlayerState.cpp
@@ -14,6 +14,11 @@ AAuraPlayerState::AAuraPlayerState()
 
     AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
     AbilitySystemComponent->SetIsReplicated(true);
+    // 设置能力系统组件的游戏玩法效果复制模式为混合模式（Mixed）。
+    // 在混合模式下，服务器会复制完整的游戏玩法效果（Gameplay Effect）给拥有该 Actor 的客户端，
+    // 而对于其他客户端，服务器仅复制游戏玩法效果的预测性快照，这样可以在保证游戏体验的同时，
+    // 有效减少网络带宽的占用。
+    AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
     AttributeSet = CreateDefaultSubobject<UAttributeSet>(TEXT("AttributeSet"));
 }
