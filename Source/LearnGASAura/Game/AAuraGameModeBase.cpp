cf98bc663cb652c9a55fc46bfcd0223758ccf43e
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-13 19:15:19 +0800

    Aura Player State

--- Source/LearnGASAura/Game/AAuraGameModeBase.cpp
+++ Source/LearnGASAura/Game/AAuraGameModeBase.cpp
@@ -4,11 +4,17 @@
 #include "AAuraGameModeBase.h"
 
 #include "LearnGASAura/Player/AuraPlayerController.h"
+#include "LearnGASAura/Player/AuraPlayerState.h"
 
 AAAuraGameModeBase::AAAuraGameModeBase()
 {
     if (const ConstructorHelpers::FClassFinder<AAuraPlayerController> AuraPlayerControllerClass(TEXT("/Game/Player/BP_AuraPlayerController.BP_AuraPlayerController_C")); IsValid(AuraPlayerControllerClass.Class))
     {
         PlayerControllerClass = AuraPlayerControllerClass.Class;
+    }
+
+    if (const ConstructorHelpers::FClassFinder<AAuraPlayerState> AuraPlayerStateClass(TEXT("/Game/Player/BP_AuraPlayerState.BP_AuraPlayerState_C")); IsValid(AuraPlayerStateClass.Class))
+    {
+        PlayerStateClass = AuraPlayerStateClass.Class;
     }
 }
---
+++ Source/LearnGASAura/Player/AuraPlayerState.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+    // 设置网络更新频率为 100.0 赫兹，这意味着该 Actor 的网络状态每秒会更新 100 次。
+    // 较高的更新频率可以让网络上的其他客户端更及时地获取该 Actor 的状态变化，
+    // 但同时也会增加网络带宽的使用。
+    SetNetUpdateFrequency(100.0f); 
+}
