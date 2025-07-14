b369dda78c652b152dcf846cbd14a39619c73933
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-14 19:38:11 +0800

    Health Mana

--- Source/LearnGASAura/AbilitySystem/AuraAttributeSet.cpp
+++ Source/LearnGASAura/AbilitySystem/AuraAttributeSet.cpp
@@ -2,3 +2,44 @@
 
 
 #include "AuraAttributeSet.h"
+
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UAuraAttributeSet::UAuraAttributeSet() {}
+
+// FLifetimeProperty 类主要用于管理在 Actor 通道生命周期内需要复制的属性，同时支持通过额外条件来控制复制行为，帮助开发者更灵活、高效地处理网络属性复制。
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    // 类名：UAuraAttributeSet（当前属性集类）
+    // 属性名：Health（要复制的属性）
+    // 复制条件：COND_None（无条件复制，总是复制）
+    // 通知方式：REPNOTIFY_Always（总是调用 OnRep 函数，即使新值与旧值相同，也会触发复制通知）
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+    // 当 Health 属性从服务器复制到客户端时，此宏会触发属性变更通知，让客户端更新 Health 属性值，同时广播属性变更事件，让监听 Health 属性的系统做出响应。
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/LearnGASAura/AbilitySystem/AuraAttributeSet.h
+++ Source/LearnGASAura/AbilitySystem/AuraAttributeSet.h
@@ -12,5 +12,32 @@ class LEARNGASAURA_API UAuraAttributeSet : public UAttributeSet
 {
     GENERATED_BODY()
 
+public:
+    UAuraAttributeSet();
 
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Health, Category= "重要属性")
+    FGameplayAttributeData Health;
+
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_MaxHealth, Category= "重要属性")
+    FGameplayAttributeData MaxHealth;
+
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_Mana, Category= "重要属性")
+    FGameplayAttributeData Mana;
+
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing= OnRep_MaxMana, Category= "重要属性")
+    FGameplayAttributeData MaxMana;
+
+    UFUNCTION()
+    void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+    UFUNCTION()
+    void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+    UFUNCTION()
+    void OnRep_Mana(const FGameplayAttributeData& OldMana);
+
+    UFUNCTION()
+    void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
 };
