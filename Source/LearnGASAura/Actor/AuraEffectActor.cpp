58bb52138bbc3efae16e2b42e1c608d7944ad44d
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-17 14:58:20 +0800

    Aura Effect Actor

---
+++ Source/LearnGASAura/Actor/AuraEffectActor.cpp
@@ -0,0 +1,42 @@
+﻿#include "AuraEffectActor.h"
+
+#include "AbilitySystemInterface.h"
+#include "Components/SphereComponent.h"
+#include "LearnGASAura/AbilitySystem/AuraAttributeSet.h"
+
+
+AAuraEffectActor::AAuraEffectActor()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+    SetRootComponent(Mesh);
+
+    Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+    Sphere->SetupAttachment(Mesh);
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+    Super::BeginPlay();
+    Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnSphereOverlap);
+    Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::OnSphereEndOverlap);
+}
+
+void AAuraEffectActor::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (const IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+    {
+        if (ASCInterface->GetAbilitySystemComponent())
+        {
+            if (const UAuraAttributeSet* AuraAttribute = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass())))
+            {
+                UAuraAttributeSet* AuraAttributes = const_cast<UAuraAttributeSet*>(AuraAttribute);
+                AuraAttributes->SetHealth(AuraAttribute->GetHealth() + 10.f);
+                Destroy();
+            }
+        }
+    }
+}
+
+void AAuraEffectActor::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {}
---
+++ Source/LearnGASAura/Actor/AuraEffectActor.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class LEARNGASAURA_API AAuraEffectActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AAuraEffectActor();
+
+protected:
+    UFUNCTION()
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<USphereComponent> Sphere;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UStaticMeshComponent> Mesh;
+
+};
