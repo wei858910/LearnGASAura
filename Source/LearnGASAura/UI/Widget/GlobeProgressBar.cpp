f6957966b53526974b3d76f378852c737ad43aa9
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-18 15:54:59 +0800

    GlobeProgressBar

--- Source/LearnGASAura/UI/Widget/GlobeProgressBar.cpp
+++ Source/LearnGASAura/UI/Widget/GlobeProgressBar.cpp
@@ -18,16 +18,32 @@ void UGlobeProgressBar::UpdateBackgroundBrush()
 void UGlobeProgressBar::SetGlobeImage() const
 {
     UWidgetLayoutLibrary::SlotAsOverlaySlot(GlobeProgressBar.Get())->SetPadding(FMargin(GlobePadding));
+    UWidgetLayoutLibrary::SlotAsOverlaySlot(GlobeProgressBar.Get())->SetHorizontalAlignment(EHorizontalAlignment::HAlign_Fill);
+    UWidgetLayoutLibrary::SlotAsOverlaySlot(GlobeProgressBar.Get())->SetVerticalAlignment(EVerticalAlignment::VAlign_Fill);
+
     GlobeProgressBar.Get()->SetFillColorAndOpacity(FLinearColor(1.0f, 1.0f, 1.0f, 1.0f));
-    
-    FProgressBarStyle ProgressBarStyle{GlobeProgressBar.Get()->GetWidgetStyle()};
+
+    FProgressBarStyle ProgressBarStyle{ GlobeProgressBar.Get()->GetWidgetStyle() };
     ProgressBarStyle.SetBackgroundImage(ProgressBarBackgroundImage);
     ProgressBarStyle.SetFillImage(ProgressBarFillImage);
-    
+
     GlobeProgressBar.Get()->SetWidgetStyle(ProgressBarStyle);
     GlobeProgressBar.Get()->SetPercent(0.5f);
     GlobeProgressBar.Get()->SetBarFillStyle(EProgressBarFillStyle::Mask);
     GlobeProgressBar.Get()->SetBarFillType(EProgressBarFillType::BottomToTop);
+
+}
+
+void UGlobeProgressBar::UpdateGlassImage()
+{
+    if (UMaterialInstance* InMaterial = LoadObject<UMaterialInstance>(nullptr, TEXT("/Game/Assets/UI/Globes/MI_EmptyGlobe.MI_EmptyGlobe")))
+    {
+        GlassBrush.SetResourceObject(InMaterial);
+        Image_Glass.Get()->SetBrush(GlassBrush);
+        UWidgetLayoutLibrary::SlotAsOverlaySlot(Image_Glass.Get())->SetPadding(FMargin(GlassPadding));
+        UWidgetLayoutLibrary::SlotAsOverlaySlot(Image_Glass.Get())->SetHorizontalAlignment(EHorizontalAlignment::HAlign_Fill);
+        UWidgetLayoutLibrary::SlotAsOverlaySlot(Image_Glass.Get())->SetVerticalAlignment(EVerticalAlignment::VAlign_Fill);
+    }
 }
 
 void UGlobeProgressBar::NativePreConstruct()
@@ -37,7 +53,7 @@ void UGlobeProgressBar::NativePreConstruct()
     UpdateBoxSize();
     UpdateBackgroundBrush();
     SetGlobeImage();
-   
+    UpdateGlassImage();
 }
 
 void UGlobeProgressBar::UpdateBoxSize() const
--- Source/LearnGASAura/UI/Widget/GlobeProgressBar.h
+++ Source/LearnGASAura/UI/Widget/GlobeProgressBar.h
@@ -19,34 +19,52 @@ public:
     UPROPERTY(meta=(BindWidget))
     TObjectPtr<USizeBox> SizeBox_Root{};
 
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="SizeBox_Root")
+    float BoxWidth{};
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="SizeBox_Root")
+    float BoxHeight{};
+
     UPROPERTY(meta=(BindWidget))
     TObjectPtr<UImage> Image_Background{};
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Image_Background")
+    FSlateBrush BackgroundBrush{};
 
     UPROPERTY(meta=(BindWidget))
     TObjectPtr<UProgressBar> GlobeProgressBar{};
 
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    float BoxWidth{};
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    float BoxHeight{};
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    FSlateBrush BackgroundBrush{};
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="GlobeProgressBar")
     FSlateBrush ProgressBarBackgroundImage{};
 
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="GlobeProgressBar")
     FSlateBrush ProgressBarFillImage{};
 
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    float GlobePadding{10.0f};
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="GlobeProgressBar")
+    float GlobePadding{ 10.0f };
 
+    UPROPERTY(meta=(BindWidget))
+    TObjectPtr<UImage> Image_Glass{};
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Image_Glass")
+    float GlassPadding{ 5.0f };
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Image_Glass")
+    FSlateBrush GlassBrush{};
+
+public:
+    UFUNCTION(BlueprintCallable)
     void UpdateBackgroundBrush();
+
+    UFUNCTION(BlueprintCallable)
     void SetGlobeImage() const;
+
+    UFUNCTION(BlueprintCallable)
+    void UpdateGlassImage();
+
+    UFUNCTION(BlueprintCallable)
     virtual void NativePreConstruct() override;
 
-protected:
+    UFUNCTION(BlueprintCallable)
     void UpdateBoxSize() const;
 };
