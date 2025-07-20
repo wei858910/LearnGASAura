cc77eece2c682c256c4879a58fb1758633fe606f
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-07-20 14:22:59 +0800

    Aura HUD

--- Source/LearnGASAura/Game/AAuraGameModeBase.cpp
+++ Source/LearnGASAura/Game/AAuraGameModeBase.cpp
@@ -3,18 +3,19 @@
 
 #include "AAuraGameModeBase.h"
 
-#include "LearnGASAura/Player/AuraPlayerController.h"
-#include "LearnGASAura/Player/AuraPlayerState.h"
+// #include "LearnGASAura/Player/AuraPlayerController.h"
+// #include "LearnGASAura/Player/AuraPlayerState.h"
+// #include "LearnGASAura/UI/HUD/AuraHUD.h"
 
 AAAuraGameModeBase::AAAuraGameModeBase()
 {
-    if (const ConstructorHelpers::FClassFinder<AAuraPlayerController> AuraPlayerControllerClass(TEXT("/Game/Bluepint/Player/BP_AuraPlayerController.BP_AuraPlayerController_C")); IsValid(AuraPlayerControllerClass.Class))
-    {
-        PlayerControllerClass = AuraPlayerControllerClass.Class;
-    }
-
-    if (const ConstructorHelpers::FClassFinder<AAuraPlayerState> AuraPlayerStateClass(TEXT("/Game/Bluepint/Player/BP_AuraPlayerState.BP_AuraPlayerState_C")); IsValid(AuraPlayerStateClass.Class))
-    {
-        PlayerStateClass = AuraPlayerStateClass.Class;
-    }
+    // if (const ConstructorHelpers::FClassFinder<AAuraPlayerController> AuraPlayerControllerClass(TEXT("/Game/Bluepint/Player/BP_AuraPlayerController.BP_AuraPlayerController_C")); IsValid(AuraPlayerControllerClass.Class))
+    // {
+    //     PlayerControllerClass = AuraPlayerControllerClass.Class;
+    // }
+    //
+    // if (const ConstructorHelpers::FClassFinder<AAuraPlayerState> AuraPlayerStateClass(TEXT("/Game/Bluepint/Player/BP_AuraPlayerState.BP_AuraPlayerState_C")); IsValid(AuraPlayerStateClass.Class))
+    // {
+    //     PlayerStateClass = AuraPlayerStateClass.Class;
+    // }
 }
---
+++ Source/LearnGASAura/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "LearnGASAura/UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if(OverlayWidgetClass)
+    {
+        if(UUserWidget* Widget = CreateWidget(GetWorld(), OverlayWidgetClass))
+        {
+            Widget->AddToViewport();
+        }
+    }
+}
